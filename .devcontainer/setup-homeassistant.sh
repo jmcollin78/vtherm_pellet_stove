73b0adbd38957d61dbe35a64df381519747ee9b8
Jean-Marc Collin <jean-marc.collin-extern@renault.com>
2026-04-28 10:21:02 +0000

    Add test configuration

--- .devcontainer/setup-homeassistant.sh
+++ .devcontainer/setup-homeassistant.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 mkdir -p .homeassistant
+ln -sfn ../.devcontainer/automations.yaml .homeassistant/automations.yaml
+ln -sfn ../.devcontainer/configuration.yaml .homeassistant/configuration.yaml
 ln -sfn ../custom_components .homeassistant/custom_components
 ln -sfn ../../.devcontainer/lovelace_dashboards .homeassistant/.storage/lovelace_dashboards
 ln -sfn ../../.devcontainer/lovelace.pellet_stove .homeassistant/.storage/lovelace.pellet_stove
